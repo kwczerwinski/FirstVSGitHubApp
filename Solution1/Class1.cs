45208a41eeef947c9a0c85eb5b8f0f94a2217822
Krzysztof Czerwinski <kwczerwinski@outlook.com>
2017-08-15 12:51:04 +0200

    Creation of first console app

---
+++ Solution1/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Class1
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello World");
+		Console.ReadLine();
+	}
+}
---
+++ Solution1/FirstCSProject/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSProject
+{
+    class HelloWorld
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
